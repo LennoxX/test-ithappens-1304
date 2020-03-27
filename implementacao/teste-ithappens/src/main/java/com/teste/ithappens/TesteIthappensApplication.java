76c6356efde28a47a2d6aec9aba1bb40b473bbc8
Lucas <lucasraphael.fernandes@hotmail.com>
2020-03-27 13:08:31 -0300

    Criação do projeto base em Java Spring Boot

---
+++ implementacao/teste-ithappens/src/main/java/com/teste/ithappens/TesteIthappensApplication.java
@@ -0,0 +1,13 @@
+package com.teste.ithappens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteIthappensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteIthappensApplication.class, args);
+	}
+
+}
